6b7d030f247da87fb0848d0fbbda63277f90b660
KishorSK <123144600+KishorSK@users.noreply.github.com>
2026-07-04 11:15:17 +0530

    removed unused imports

--- SugarFitAppium/src/test/java/com/sugarfit/pages/HomePage.java
+++ SugarFitAppium/src/test/java/com/sugarfit/pages/HomePage.java
@@ -4,9 +4,6 @@ import java.util.List;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
-import org.openqa.selenium.support.ui.ExpectedConditions;
-import org.openqa.selenium.support.ui.WebDriverWait;
-import java.time.Duration;
 import io.appium.java_client.AppiumBy;
 import io.appium.java_client.android.AndroidDriver;
 
